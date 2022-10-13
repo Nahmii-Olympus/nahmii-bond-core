146b32bad55ee3324738687930d2ce9a6e0ddbef
CodingCas <casweeno2000@gmail.com>
2022-10-13 23:50:19 +0100

    Casweeney: added env.example

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config({ path: ".env" });
+
+
+const ALCHEMY_GOERLI_API_KEY_URL = process.env.ALCHEMY_GOERLI_API_KEY_URL;
+
+const API_TOKEN = process.env.API_TOKEN;
+
+const NAHMII_URL = process.env.NAHMII_URL;
+
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
 
 module.exports = {
   solidity: {
@@ -15,15 +25,15 @@ module.exports = {
   },
   networks: {
     nahmii: {
-      url: "https://ngeth.n3g0.nahmii.net",
-      accounts: ["852faf46019bdaaa1cebe9e651baa1c17304926900e7952ef586d247cf9910e1"]
+      url: NAHMII_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY]
     },
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/iWVVGEdMfq5za6NeV_U0W86woka0EQow",
-      accounts: ["852faf46019bdaaa1cebe9e651baa1c17304926900e7952ef586d247cf9910e1"]
+      url: ALCHEMY_GOERLI_API_KEY_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY],
     }
   },
   etherscan: {
-    apiKey: "M5K1IXBCD394915V37W5IT27HGU8WNTZE9"
-  },
+    apiKey: API_TOKEN
+  }
 };
